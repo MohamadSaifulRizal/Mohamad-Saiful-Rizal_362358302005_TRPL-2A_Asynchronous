ad14d1d8c7e0fc962e89c20e7f9b315ca54273e8
MOHAMAD SAIFUL RIZAL <rizalbwi201@gmail.com>
2024-11-26 15:22:53 +0700

    capek aku tuch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
+import 'package:books/navigation_dialog.dart';
+import 'package:books/navigation_first.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:async/async.dart';
+// import 'package:async/async.dart';
+import 'package:books/geolocation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +23,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const FuturePage(),
+      // home: const FuturePage(),
+      // home: LocationScreen(),
+      // home: const NavigationFirst(),
+      home: const NavigationDialogScreen(),
     );
   }
 }
@@ -30,7 +39,95 @@ class FuturePage extends StatefulWidget {
 }
 
 class _FuturePageState extends State<FuturePage> {
+
+late Completer completer;
+
+Future getNumber() {
+  completer = Completer<int>();
+  // calculate();
+  return completer.future;
+
+  calculate() async {
+  try {
+    await new Future.delayed (const Duration (seconds : 5));
+    completer.complete(42);
+// throw Exception();
+  }
+  catch (_) {
+    completer.completeError({});
+  }
+}
+}
+
+Future returnError() async {
+  await Future.delayed(const Duration(seconds: 2));
+  throw Exception('something terrible happened!');
+}
+
+Future calculate() async {
+  await Future.delayed(const Duration(seconds : 5));
+  completer.complete(42);
+}
+
   String result = '';
+  void returnFG() {
+    Future.wait<int>([
+  returnOneAsync(),
+  returnTwoAsync(),
+  returnThreeAsync(),
+]);
+    FutureGroup<int> futureGroup = FutureGroup<int>();
+    futureGroup.add(returnOneAsync());
+    futureGroup.add(returnTwoAsync());
+    futureGroup.add(returnThreeAsync());
+    futureGroup.close();
+    futureGroup.future.then((List<int> value) {
+      int total = 0;
+      for (var element in value) {
+        total += element;
+    }
+    setState(() {
+      result = total.toString();
+    });
+  });
+}
+
+
+  Future<Response> getData() async {
+  const authority = 'www.googleapis.com';
+  const path = '/books/v1/volumes/j1W6EAAAQBAJ';
+  Uri url = Uri.https (authority, path);
+  return http.get(url);
+}
+
+Future<int> returnOneAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 1;
+}
+
+Future<int> returnTwoAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 2;
+}
+
+Future<int> returnThreeAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 3;
+}
+
+
+
+Future count() async {
+  int total = 0;
+  total = await returnOneAsync();
+  total += await returnTwoAsync();
+  total += await returnThreeAsync();
+  setState(() {
+    result = total.toString();
+  });
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,10 +137,53 @@ class _FuturePageState extends State<FuturePage> {
       body: Center(
         child: Column(children: [
           const Spacer(),
-          ElevatedButton(
-            child: const Text('GO!'),
-            onPressed: () {},
-          ),
+          // ElevatedButton(
+          //   child: const Text('GO!'),
+          //   onPressed: () {
+          //    setState(() {});
+          //    getData()
+          //    .then((value) {
+          //       result = value.body.toString().substring(0, 450);
+          //       setState(() {});
+          //    }).catchError((_) {
+          //     result = 'An error occurred';
+          //     setState(() {});
+          //    });
+          //   },
+          // ),
+
+ElevatedButton(
+  child: Text('GO!'),
+    onPressed: () {
+  returnFG();
+  // returnError() 
+  // .then((value) {
+  //   setState(() {
+  //    result =  'succes'; 
+  //   });
+  // }).catchError((onError) {
+  //   setState(() {
+  //     result = onError.toString();
+  //   });
+  // }).whenComplete(() => print('complete'));
+Future handleError() async {
+  try {
+    await returnError();
+  }
+  catch (error) {
+    setState(() {
+      result = error.toString();
+    });
+  }
+  finally {
+    print('complete');
+  }
+}
+  },
+),
+
+
+
           const Spacer(),
           Text(result),
           const Spacer(),
@@ -54,3 +194,81 @@ class _FuturePageState extends State<FuturePage> {
     );
   }
 }
+
+
+// count();
+  //   getNumber().then((value) {
+  // setState(() {
+  //   result = value.toString();
+  //   });
+  // });
+//   getNumber().then((value) {
+//   setState(() {
+//     result = value.toString();
+//   });
+// }).catchError((e) {
+//   result = 'An error occurred';
+// });
+
+// ElevatedButton(
+//   child: Text('GO!'),
+//     onPressed: () {
+//     count();
+//   },
+// }
+
+
+// getNumber().then((value) {
+//   setState(() {
+//     result = value.toString();
+//     });
+//   });
+
+
+// calculate() async {
+//   try {
+//     await new Future.delayed (const Duration (seconds : 5));
+//     completer.complete(42);
+// // throw Exception();
+//   }
+//   catch (_) {
+//     completer.completeError({});
+//   }
+// }
+
+
+// void returnFG() {
+//     FutureGroup<int> futureGroup FutureGroup<int>();
+//     futureGroup.add(returnOneAsync());
+//     futureGroup.add(returnTwoAsync());
+//     futureGroup.add(returnThreeAsync());
+//     futureGroup.close();
+//     futureGroup.future.then((List<int> value) {
+//       int total = 0;
+//       for (var element in value) {
+//         total += element;
+//     }
+//     setState(() {
+//       result = total.toString();
+//     });
+//   });
+// }
+
+// Future returnError() async {
+//   await Future.delayed(const Duration(seconds: 2));
+//   throw Exception('something terrible happened!');
+// }
+
+// Future handleError() async {
+//   try {
+//     await returnError();
+//   }
+//   catch (error) {
+//     setState(() {
+//       result = error.toString();
+//     });
+//   }
+//   finally {
+//     print('complete');
+//   }
+// }
